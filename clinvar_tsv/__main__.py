2ba4da530f1a154ebf1969e7e0040dd7e482a65d
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2018-11-12 09:15:56 +0100

    Adding --cores parameter to clinvar_tsv CLI

--- clinvar_tsv/__main__.py
+++ clinvar_tsv/__main__.py
@@ -31,6 +31,7 @@ def run_main(args):
         "printshellcmds": True,
         "verbose": True,
         "force_incomplete": True,
+        "cores": 8,
     }
     return snakemake.snakemake(**kwargs)
 
@@ -97,6 +98,7 @@ def main(argv=None):
         "--b38-path", required=True, help="Path to GRCh38 FAI-indexed FASTA file."
     )
     parser_main.add_argument("--work-dir", default=os.getcwd(), help="Path to working directory")
+    parser_main.add_argument("--cores", default=1, type=int, help="Number of cores to use")
     parser_main.add_argument(
         "--debug", default=False, action="store_true", help="Enables debugging helps"
     )
